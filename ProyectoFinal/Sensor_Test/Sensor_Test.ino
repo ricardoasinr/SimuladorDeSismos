3ecbfd3e1e49d49e964edf7916e50f02be2e55a9
Ricardo Asin <ricardoasinr@gmail.com>
2022-05-13 23:45:19 -0400

    Tabulando datos

--- ProyectoFinal/Sensor_Test/Sensor_Test.ino
+++ ProyectoFinal/Sensor_Test/Sensor_Test.ino
@@ -18,39 +18,50 @@ void loop() {
   // Update gyro calibration 
   imu.updateBias();
   
-  Serial.println("");
+  Serial.println("****************");
+  Serial.println( "" );
   Serial.print("Temperatura: ");
   Serial.print( imu.temp() );
   Serial.println(); 
+  Serial.println( "----------------" );
   
  
   //-- Scaled and calibrated output:
   // Accel
   Serial.println("");
-  Serial.println("Acelerometro");
-  Serial.print("x: ");
-  Serial.print( imu.ax() );
-  Serial.println( " " );
-   Serial.print("y: ");
-  Serial.print( imu.ay() );
-  Serial.println( " " );
-   Serial.print("z: ");
-  Serial.print( imu.az() );
-  Serial.println( " " );
-  delay(2000);
+  
+  Serial.println ("Acelerometro:"); 
+  
+  Serial.print("x: ");  
+  Serial.print( imu.ax() ); Serial.print("\t"); 
 
-  Serial.println("");
-  Serial.println("Giroscopio");
-  Serial.print("x: ");
-  Serial.print( imu.gx() );
-  Serial.println( " " );
    Serial.print("y: ");
-  Serial.print( imu.gy() );
-  Serial.println( " " );
-   Serial.print("z: ");
-  Serial.print( imu.gz() );
-  Serial.println( " " );
-  delay(2000);
+  Serial.print( imu.ay() ); Serial.print("\t"); 
+  Serial.print("\t"); 
+
+   Serial.print("z: "); 
+  Serial.print( imu.az() );Serial.println("\t"); 
+  
+  Serial.println( "----------------" );
+  
+  
+  Serial.println ("Giroscopio:"); 
+  
+  Serial.print("x: ");  
+  Serial.print( imu.gx() ); Serial.print("\t"); 
+
+  Serial.print("y: ");
+  Serial.print( imu.gy() ); Serial.print("\t"); 
+  Serial.print("\t"); 
+
+  Serial.print("z: "); 
+  Serial.print( imu.gz() );Serial.println("\t"); 
+  
+  Serial.println( "----------------" );
+  Serial.println("");
+
+  delay(300);
+
 
   
   
