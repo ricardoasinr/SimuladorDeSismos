b25a62697f7bbad8170b8220cd5f14e8fa231c57
Mateo Fernandez <mateo.fersal1203@gmail.com>
2022-06-20 17:10:03 -0400

    codigo con manejo de bluetooth

--- Menu_Mateo/Menu_Mateo.ino
+++ Menu_Mateo/Menu_Mateo.ino
@@ -13,57 +13,36 @@ Servo servo2;
 Adafruit_SSD1306 oled(ANCHO, ALTO, &Wire, OLED_RESET);  // crea objeto
 
 #include <Keypad.h> // importa o incluye la libreria Keypad
-const byte FILAS=3;
+const byte FILAS=2;
 const byte COLUMNAS=4;
 char keys[FILAS][COLUMNAS]= 
 {    
   {'1','2', '3', 'A'},
-  {'4','5', '6', 'B'},
-  {'*','0', '#', 'D'}
+  {'4','5', '6', 'B'}
 };
-byte pinesFilas[FILAS]= {8,9, 4};
+byte pinesFilas[FILAS]= {8,9};
 byte pinesColumnas[COLUMNAS]= {10,11,12,13};
 Keypad teclado = Keypad(makeKeymap(keys), pinesFilas, pinesColumnas, FILAS, COLUMNAS);
 char tecla;
 char aux;
-char sub_aux;
-int seleccion_duraccion(char sub_aux);
 
 int sw;
 #define sw1 7
 
+int buzzer_pin= 2;
+
 int intensidad = 0;
 int duracion = 0;
-int fin=0;
-int t=0;
+unsigned int fin=0;
+unsigned int t=0;
+unsigned int factor = 0;
 float val_intensidad = 0;
+
+String s; 
+char x;
 
 
 // AREA DE SUBPROGRAMAS PARA MEL MENU
-
-
-void menu_inicial(){
-
-  oled.clearDisplay();
-  oled.setTextColor(WHITE);
-  oled.setCursor(32,0);
-  oled.setTextSize(1);
-  oled.println("Menu inicial");
-  oled.println("\nModos de manejo: ");
-  oled.println("\n1.  Bluetooth");  
-  oled.println("\n2.  Manual");
-  oled.display();
-}
-
-void menu_apagado(){
-
-  oled.clearDisplay();
-  oled.setTextColor(WHITE);   
-  oled.setCursor(0, 30);     
-  oled.setTextSize(1);      
-  oled.print("Sistema: Apagado");
-  oled.display();
-}
 
 void menu_bt(){
 
@@ -130,6 +109,14 @@ void  menu_duracion(){
   oled.display();
 }
 
+void sonido(){
+
+  tone(buzzer_pin, 1700);
+  delay(200);
+  noTone(buzzer_pin);
+  
+}
+
 void muestra_parametros(int val_intesidad, int duracion){
 
   oled.clearDisplay();
@@ -167,25 +154,39 @@ void muestra_parametros(int val_intesidad, int duracion){
   oled.display();
 }
 
-void muestra_simulacion(int t){
-
-  
-  
-}
-
 void simulacion(int intensidad){
   
-    for(int k=0; k<= 180; k++){
-
-      servo1.write(k); servo2.write(k); 
+    for(int k=0; k<= 110; k++){
+      servo2.write(k); servo1.write(k); 
       delay(intensidad);
     }
 }
 
 void volver(){
+  sonido();
   tecla='X'; 
 }
 
+void sismo_en_curso(){
+
+  oled.clearDisplay();
+  oled.setTextColor(WHITE);
+  oled.setCursor(10,30);
+  oled.setTextSize(1);
+  oled.print("SISMO EN CURSO");
+  oled.display();
+}
+
+int factor_tiempo(int intensidad){
+  
+  switch(intensidad){
+
+    case 4: return 125;
+    case 3: return 90;
+    case 2: return 60;
+    case 1: return 30;
+  }
+}
 
 void setup() { 
   
@@ -194,30 +195,35 @@ void setup() {
   oled.begin(SSD1306_SWITCHCAPVCC, 0x3C); // inicializa pantalla con direccion 0x3C
   oled.clearDisplay();
   oled.display();
-  servo1.attach(5); servo2.attach(5);
+  servo1.attach(5); servo2.attach(6);
+  pinMode(4, OUTPUT);
   }
  
 void loop() {
 
-  Serial.println(fin);
+  //Serial.println(sw);
   sw = digitalRead(sw1);
   
   if(sw==0){
 
     menu_manual();
     tecla = teclado.getKey();
+
+    if(tecla){
+      sonido();
    
     while(tecla=='1'){
 
+       
        menu_intensidad();
        aux = teclado.getKey();
 
        switch(aux){
 
-              case '1' : intensidad = 5; val_intensidad = 2; break;
-              case '2' : intensidad = 4; val_intensidad = 3.5; break;
-              case '3' : intensidad = 3; val_intensidad = 5; break;
-              case '4' : intensidad = 1; val_intensidad = 7; break;
+              case '1' : sonido();intensidad = 4; val_intensidad = 2; break;
+              case '2' : sonido();intensidad = 3; val_intensidad = 3.5; break;
+              case '3' : sonido();intensidad = 2; val_intensidad = 5; break;
+              case '4' : sonido();intensidad = 1; val_intensidad = 7; break;
               case 'A' : volver(); break;
           }
     }
@@ -229,10 +235,10 @@ void loop() {
 
        switch(aux){
 
-              case '1' : duracion = 15; fin=duracion*1000; break;
-              case '2' : duracion = 30; fin=duracion*1000;break;
-              case '3' : duracion = 45; fin=duracion*1000;break;
-              case '4' : duracion = 60; fin=duracion*1000;break;
+              case '1' : sonido(); duracion = 15; fin=duracion*155; break;
+              case '2' : sonido(); duracion = 30; fin=duracion*157;break;
+              case '3' : sonido(); duracion = 45; fin=duracion*160;break;
+              case '4' : sonido(); duracion = 60; fin=duracion*162;break;
               case 'A' : volver(); break;
       }      
     }
@@ -249,12 +255,14 @@ void loop() {
       }
       while(tecla=='B'){
 
-        while(t  <=  fin ){
+        sismo_en_curso();
+        factor = factor_tiempo(intensidad); Serial.println(factor);
+        while(t  <=  fin){
 
-          Serial.println(t);
           simulacion(intensidad);
-          t+=1000;
+          t+=factor;
       }   
+      factor = 0;
       t=0;
       duracion=0;
       intensidad=0;
@@ -262,9 +270,79 @@ void loop() {
       volver();
     }
   }
+ }
   else{
 
       menu_bt();
+      while(Serial.available()>0){
       
+         delay(10);
+         x = Serial.read(); 
+         s+=x;
+      }
+      if(s.length() > 0){
+
+         s.toLowerCase();
+         Serial.println(s);
+         
+         if(s=="modo 1"){
+
+            intensidad = 4; 
+            duracion = 15;
+            fin=duracion*160;
+            sismo_en_curso();
+            factor = factor_tiempo(intensidad);
+            while(t  <=  fin){
+
+              simulacion(intensidad);
+              t+=factor;
+            }
+            t=0;   
+         }
+         if(s=="modo 2"){
+          
+           intensidad = 3; 
+           duracion = 15;
+           fin=duracion*160;
+           sismo_en_curso();
+           factor = factor_tiempo(intensidad);
+           while(t  <=  fin){
+
+              simulacion(intensidad);
+              t+=factor;
+           }   
+           t=0;
+         }
+         
+         if(s=="modo 3"){
+           intensidad = 2; 
+           duracion = 15;
+           fin=duracion*160;
+           sismo_en_curso();
+           factor = factor_tiempo(intensidad);
+           while(t  <=  fin){
+
+              simulacion(intensidad);
+              t+=factor;
+           }
+           t=0;   
+         }
+         
+         if(s=="modo 4"){
+            
+           intensidad = 1; 
+           duracion = 30;
+           fin=duracion*166;
+           sismo_en_curso();
+           factor = factor_tiempo(intensidad);
+           while(t  <=  fin){
+
+              simulacion(intensidad);
+              t+=factor;
+           }
+           t=0;
+          }
+    }
+    s="";
   }
 }
